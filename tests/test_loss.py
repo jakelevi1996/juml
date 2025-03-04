af3626c34d5630013d5cecd609b80e57b61f4809
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-04 13:17:43 +0000

    Add `test_mse` unit test

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -27,3 +27,24 @@ def test_crossentropy():
     assert isinstance(metric, int)
     assert metric >= 0
     assert metric < batch_size
+
+def test_mse():
+    printer = util.Printer("test_mse", dir_name=OUTPUT_DIR)
+    juml_test_utils.set_torch_seed("test_mse")
+
+    output_dim = 11
+    batch_size = 87
+
+    y = torch.normal(0, 1, [batch_size, output_dim])
+    t = torch.normal(0, 1, [batch_size, output_dim])
+
+    mse_loss = juml.datasets.loss.Mse()
+
+    loss = mse_loss.forward(y, t)
+    assert isinstance(loss, torch.Tensor)
+    assert list(loss.shape) == []
+    assert loss.item() >= 0
+
+    metric = mse_loss.metric_batch(y, t)
+    assert isinstance(metric, float)
+    assert metric >= 0
