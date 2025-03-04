15e172b1ef980a4844d28ee10176577a0bbec9ea
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-04 16:06:47 +0000

    Add `juml.datasets.Cifar10`

--- src/juml/datasets/cifar10.py
+++ src/juml/datasets/cifar10.py
@@ -7,9 +7,9 @@ from juml.datasets.base import (
     DATA_REL_DIR,
 )
 
-class Mnist(Dataset):
+class Cifar10(Dataset):
     def get_data_split(self, split: str) -> torch.utils.data.Dataset:
-        return torchvision.datasets.MNIST(
+        return torchvision.datasets.CIFAR10(
             root=DATA_REL_DIR,
             train=SPLIT_STR_TO_TRAIN_BOOL[split],
             transform=torchvision.transforms.ToTensor(),
@@ -17,7 +17,7 @@ class Mnist(Dataset):
         )
 
     def get_input_shape(self) -> list[int]:
-        return [1, 28, 28]
+        return [3, 32, 32]
 
     def get_output_shape(self) -> list[int]:
         return [10]
