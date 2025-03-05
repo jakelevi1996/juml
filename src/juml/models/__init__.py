816e33f190fa745c58437e33fb5cf1cfceb48171
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-05 12:54:44 +0000

    In `juml.models` namespace include `LinearModel` but not `LinearLayer, MultiHeadLinear`

--- src/juml/models/__init__.py
+++ src/juml/models/__init__.py
@@ -1,4 +1,4 @@
 from juml.models import embed
 from juml.models.base import Model
-from juml.models.linear import LinearLayer, MultiHeadLinear
+from juml.models.linear import LinearModel
 from juml.models.mlp import Mlp
