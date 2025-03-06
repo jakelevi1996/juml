e68268f6aaca8adc25ff911fa3d80bfca2f5b776
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-06 02:09:36 +0000

    `torch.set_printoptions` now called within the `torch_set_print_options` function

--- src/juml/test_utils.py
+++ src/juml/test_utils.py
@@ -4,12 +4,18 @@ from jutility import util
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-torch.set_printoptions(
+def torch_set_print_options(
     precision=3,
     linewidth=10000,
     sci_mode=False,
     threshold=int(1e9),
-)
+):
+    torch.set_printoptions(
+        precision=precision,
+        linewidth=linewidth,
+        sci_mode=sci_mode,
+        threshold=threshold,
+    )
 
 def get_output_dir(*subdir_names):
     return os.path.join("tests", "Outputs", *subdir_names)
