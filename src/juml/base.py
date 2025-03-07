7e8fb3b18e880fdd0cf775d86f32348d25aabac9
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-07 17:49:57 +0000

    Include `Pooler` in `juml.base` namespace

--- src/juml/base.py
+++ src/juml/base.py
@@ -1,4 +1,5 @@
 from juml.models.base import Model
 from juml.models.embed import Embedder
+from juml.models.pool  import Pooler
 from juml.datasets.base import Dataset, DataSplit
 from juml.datasets.loss import Loss
