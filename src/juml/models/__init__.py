87718a0693e850534bc14952f0ee602736775e0b
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-07 00:05:30 +0000

    Add `LinearLayer, MultiHeadLinear` to `juml.models` namespace, because they may want to be used by downstream projects

--- src/juml/models/__init__.py
+++ src/juml/models/__init__.py
@@ -1,3 +1,3 @@
 from juml.models import embed, pool
-from juml.models.linear import LinearModel
+from juml.models.linear import LinearModel, LinearLayer, MultiHeadLinear
 from juml.models.mlp import Mlp
