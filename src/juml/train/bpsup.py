9834207bfd7d4f475242ee79dcf04500c3ee43bd
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-09 01:23:57 +0000

    Add remaining CLI args to `class BpSup(Trainer):`

--- src/juml/train/bpsup.py
+++ src/juml/train/bpsup.py
@@ -7,12 +7,18 @@ from juml.datasets.base import Dataset
 class BpSup(Trainer):
     def __init__(
         self,
-        args:       cli.ParsedArgs,
-        model:      Model,
-        dataset:    Dataset,
-        optimiser:  torch.optim.Optimizer,
-        batch_size: int,
-        epochs:     int,
+        args:           cli.ParsedArgs,
+        model:          Model,
+        dataset:        Dataset,
+        optimiser:      torch.optim.Optimizer,
+        lrs:            torch.optim.lr_scheduler.LRScheduler,
+        seed:           int,
+        batch_size:     int,
+        epochs:         int,
+        gpu:            bool,
+        devices:        list[int],
+        configs:        list[str],
+        print_level:    int,
     ):
 
     @classmethod
@@ -31,8 +37,13 @@ class BpSup(Trainer):
     @classmethod
     def get_cli_options(cls) -> list[cli.Arg]:
         return [
+            cli.Arg("seed",         type=int, default=0),
             cli.Arg("batch_size",   type=int, default=100),
             cli.Arg("epochs",       type=int, default=10),
+            cli.NoTagArg("gpu",          action="store_true"),
+            cli.NoTagArg("devices",      type=int, default=[], nargs="*"),
+            cli.NoTagArg("configs",      type=str, default=[], nargs="*"),
+            cli.NoTagArg("print_level",  type=int, default=0),
             cli.ObjectChoice(
                 "optimiser",
                 cli.ObjectArg(
@@ -45,5 +56,13 @@ class BpSup(Trainer):
                     cli.Arg("weight_decay", type=float, default=0.01),
                 ),
                 default="Adam",
+            ),
+            cli.ObjectChoice(
+                "lrs",
+                cli.ObjectArg(
+                    torch.optim.lr_scheduler.CosineAnnealingLR,
+                    cli.Arg("eta_min", type=float, default=1e-5),
+                ),
+                default="CosineAnnealingLR",
             ),
         ]
