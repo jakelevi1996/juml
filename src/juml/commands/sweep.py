baa21ee4e48285d8744cf5b106727d9ecf0cb05e
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-13 17:05:48 +0000

    Instead of extracting `--devices` from `args` and checking empty, just don't include it in `sweep` CLI

--- src/juml/commands/sweep.py
+++ src/juml/commands/sweep.py
@@ -4,27 +4,18 @@ from juml.train.sweep import Sweeper
 
 class Sweep(Command):
     def run(self, args: cli.ParsedArgs):
-        kwargs  = self.get_kwargs()
-        devices = kwargs.pop("devices")
-        if len(devices) > 0:
-            raise ValueError(
-                "Received `--devices %s`, use `--Sweeper.devices` instead "
-                "with `sweep` command"
-                % devices
-            )
-
         with cli.verbose:
             sweeper = args.init_object(
                 "Sweeper",
                 args=args,
-                **kwargs,
-            )
+                **self.get_kwargs(),
+        )
 
         return sweeper
 
     @classmethod
     def get_args(cls, train_args: list[cli.Arg]) -> list[cli.Arg]:
         return [
-            *train_args,
+            *[arg for arg in train_args if arg.name != "devices"],
             Sweeper.get_cli_arg(),
         ]
