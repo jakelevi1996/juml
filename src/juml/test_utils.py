be365f84648d6088d7df079232993052146e591d
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-15 13:20:28 +0000

    Add type hints `src/juml/test_utils.py`

--- src/juml/test_utils.py
+++ src/juml/test_utils.py
@@ -5,19 +5,19 @@ from jutility import util
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def torch_set_print_options(
-    precision=3,
-    linewidth=10000,
-    sci_mode=False,
-    threshold=int(1e9),
+    precision:  int=3,
+    linewidth:  int=10000,
+    sci_mode:   bool=False,
+    threshold:  (int | float)=1e9,
 ):
     torch.set_printoptions(
         precision=precision,
         linewidth=linewidth,
         sci_mode=sci_mode,
-        threshold=threshold,
+        threshold=int(threshold),
     )
 
-def get_output_dir(*subdir_names):
+def get_output_dir(*subdir_names: str):
     return os.path.join("tests", "Outputs", *subdir_names)
 
 def set_torch_seed(*args):
