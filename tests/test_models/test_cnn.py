a3d0d864cabb7844b7da6334bf1f0dabbbb91e36
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-17 17:41:16 +0000

    Update `test_cnn` to match template

--- tests/test_models/test_cnn.py
+++ tests/test_models/test_cnn.py
@@ -4,12 +4,12 @@ import juml
 
 OUTPUT_DIR = juml.test_utils.get_output_dir("test_models/test_cnn")
 
-def test_cnn_identity_avg():
-    printer = util.Printer("test_cnn_identity_avg", dir_name=OUTPUT_DIR)
-    juml.test_utils.set_torch_seed("test_cnn_identity_avg")
+def test_cnn():
+    printer = util.Printer("test_cnn", dir_name=OUTPUT_DIR)
+    juml.test_utils.set_torch_seed("test_cnn")
 
-    x = torch.rand([3, 4, 5, 6])
-    t = torch.rand([3, 11])
+    x = torch.rand([3, 4, 17, 19])
+    t = torch.rand([3, 9, 8, 9])
 
     model = juml.models.Cnn(
         input_shape=list(x.shape),
@@ -20,16 +20,32 @@ def test_cnn_identity_avg():
         blocks_per_stage=3,
         stride=2,
         embedder=juml.models.embed.Identity(),
-        pooler=juml.models.pool.Average2d(),
+        pooler=juml.models.pool.Identity(),
     )
-    assert repr(model) == "Cnn(num_params=4.9k)"
-    assert model.num_params() == 4871
+    loss = juml.loss.Mse()
+    optimiser = torch.optim.Adam(model.parameters())
 
-    y = model.forward(x)
-    assert isinstance(y, torch.Tensor)
-    assert y.dtype is torch.float32
-    assert y.dtype is not torch.int64
-    assert list(y.shape) == [3, 11]
+    assert repr(model) == "Cnn(num_params=4.8k)"
+    assert model.num_params() == 4761
+
+    y_0 = model.forward(x)
+    assert isinstance(y_0, torch.Tensor)
+    assert y_0.dtype is torch.float32
+    assert y_0.dtype is not torch.int64
+    assert list(y_0.shape) == list(t.shape)
+    printer(y_0.max(), y_0.min())
+    assert y_0.max().item() <= 2
+    assert y_0.min().item() >= -2
+
+    loss_0 = loss.forward(y_0, t)
+    loss_0.backward()
+    optimiser.step()
+
+    y_1 = model.forward(x)
+    loss_1 = loss.forward(y_1, t)
+
+    printer(loss_0, loss_1)
+    assert loss_1.item() < loss_0.item()
 
     assert repr(model.embed) == "Identity(num_params=0)"
     assert repr(list(model.layers)) == (
@@ -38,7 +54,7 @@ def test_cnn_identity_avg():
         "ReluCnnLayer(num_params=738), ReluCnnLayer(num_params=738), "
         "ReluCnnLayer(num_params=738)]"
     )
-    assert repr(model.pool) == "Average2d(num_params=110)"
+    assert repr(model.pool) == "Identity(num_params=0)"
 
 def test_cnn_coordconv_sigprod():
     printer = util.Printer("test_cnn_coordconv_sigprod", dir_name=OUTPUT_DIR)
