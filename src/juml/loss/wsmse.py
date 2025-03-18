86cf519af325870534a2e0d9d8941a23d789388a
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-18 12:02:35 +0000

    Use `_n` consistently as named batch dimension

--- src/juml/loss/wsmse.py
+++ src/juml/loss/wsmse.py
@@ -2,13 +2,13 @@ import torch
 from juml.loss.mse import Mse
 
 class WeightedSetMse(Mse):
-    def forward(self, y_bpo: torch.Tensor, t_bpo: torch.Tensor):
-        e_bpo = self.weights * (y_bpo - t_bpo)
-        return e_bpo.square().sum(dim=-1).mean()
+    def forward(self, y_npo: torch.Tensor, t_npo: torch.Tensor):
+        e_npo = self.weights * (y_npo - t_npo)
+        return e_npo.square().sum(dim=-1).mean()
 
-    def metric_batch(self, y_bpo: torch.Tensor, t_bpo: torch.Tensor):
-        e_bpo = self.weights * (y_bpo - t_bpo)
-        return e_bpo.square().mean(dim=-2).sum().item()
+    def metric_batch(self, y_npo: torch.Tensor, t_npo: torch.Tensor):
+        e_npo = self.weights * (y_npo - t_npo)
+        return e_npo.square().mean(dim=-2).sum().item()
 
     def needs_weights(self) -> bool:
         return True
