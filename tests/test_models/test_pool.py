abfa623db614736ed5d58e438bfdb6862a58dec7
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-18 13:49:42 +0000

    Standardise `juml.models.pool.Max2d` tests

--- tests/test_models/test_pool.py
+++ tests/test_models/test_pool.py
@@ -15,15 +15,3 @@ def test_average2d():
     x = torch.rand([6, 4, 3, 2])
     y = pooler.forward(x)
     assert list(y.shape) == [6, 8]
-
-def test_max2d():
-    printer = util.Printer("test_max2d", dir_name=OUTPUT_DIR)
-    juml.test_utils.set_torch_seed("test_max2d")
-
-    pooler = juml.models.pool.Max2d()
-    pooler.set_shapes([3, 4, 5, 6], [7, 8])
-    assert repr(pooler) == "Max2d(num_params=40)"
-
-    x = torch.rand([6, 4, 3, 2])
-    y = pooler.forward(x)
-    assert list(y.shape) == [6, 8]
