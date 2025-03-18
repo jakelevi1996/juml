29a778c693152d8844a2496daf7ef3a9832beb05
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-18 19:06:22 +0000

    Update `test_crossentropy` to match `tests/templates.md`

--- tests/test_loss/test_crossentropy.py
+++ tests/test_loss/test_crossentropy.py
@@ -8,20 +8,24 @@ def test_crossentropy():
     printer = util.Printer("test_crossentropy", dir_name=OUTPUT_DIR)
     juml.test_utils.set_torch_seed("test_crossentropy")
 
-    output_dim = 11
-    batch_size = 87
+    batch_size  = 87
+    num_classes = 11
+    y = torch.rand([batch_size, num_classes])
+    t = torch.randint(0, num_classes, [batch_size])
 
-    y = torch.normal(0, 1, [batch_size, output_dim])
-    t = torch.randint(0, output_dim, [batch_size])
+    loss = juml.loss.CrossEntropy()
 
-    ce_loss = juml.loss.CrossEntropy()
+    batch_loss = loss.forward(y, t)
+    assert isinstance(batch_loss, torch.Tensor)
+    assert batch_loss.dtype is torch.float32
+    assert batch_loss.dtype is not torch.int64
+    assert list(batch_loss.shape) == []
+    printer(batch_loss)
+    assert batch_loss.item() >= 0
+    assert batch_loss.item() <= 3
 
-    loss = ce_loss.forward(y, t)
-    assert isinstance(loss, torch.Tensor)
-    assert list(loss.shape) == []
-    assert loss.item() >= 0
-
-    metric = ce_loss.metric_batch(y, t)
+    metric = loss.metric_batch(y, t)
+    printer(metric)
     assert isinstance(metric, int)
     assert metric >= 0
-    assert metric < batch_size
+    assert metric <= batch_size
