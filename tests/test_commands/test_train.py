170048a024ec9b0f06e6140e44e1525c06c2cb4f
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-19 01:40:41 +0000

    Simplify `test_train`

--- tests/test_commands/test_train.py
+++ tests/test_commands/test_train.py
@@ -9,8 +9,8 @@ def test_train():
     juml.test_utils.set_torch_seed("test_train")
 
     output_path = (
-        "results/train/dSh1i3o5te200tr200ts0.1x0.0_lM_mMeIh23n2pI_"
-        "tBb57e7lCle1E-05oAol0.001_s0/model.pth"
+        "results/train/dSh1i3o5te200tr200ts0.1x0.0_lM_mMeIh11n1pI_"
+        "tBb57e2lCle1E-05oAol0.001_s0/model.pth"
     )
     if os.path.isfile(output_path):
         os.remove(output_path)
@@ -21,11 +21,11 @@ def test_train():
     args_str = (
         "train "
         "--trainer BpSp "
-        "--trainer.BpSp.epochs 7 "
+        "--trainer.BpSp.epochs 2 "
         "--trainer.BpSp.batch_size 57 "
         "--model Mlp "
-        "--model.Mlp.hidden_dim 23 "
-        "--model.Mlp.num_hidden_layers 2 "
+        "--model.Mlp.hidden_dim 11 "
+        "--model.Mlp.num_hidden_layers 1 "
         "--dataset SinMix "
         "--dataset.SinMix.input_dim 3 "
         "--dataset.SinMix.output_dim 5 "
