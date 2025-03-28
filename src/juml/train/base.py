de7e9d3236eb400cb4c8745c458fb8a041a98d2a
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-03-28 19:16:33 +0000

    Add `Trainer.get_tag` method (that can optionally be overriden by subclasses)

--- src/juml/train/base.py
+++ src/juml/train/base.py
@@ -303,11 +303,16 @@ class Trainer:
         return cli.ObjectArg(
             cls,
             *cls.get_cli_options(),
+            tag=cls.get_tag(),
         )
 
     @classmethod
     def get_cli_options(cls) -> list[cli.Arg]:
         raise NotImplementedError()
+
+    @classmethod
+    def get_tag(cls) -> (str | None):
+        return None
 
     def __repr__(self):
         return util.format_type(type(self))
