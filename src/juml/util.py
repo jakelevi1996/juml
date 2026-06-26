d06c3e36fa548a72ceb5ef2b782c30fc8ba233ac
jakelevi1996 <jakelevi@hotmail.co.uk>
2026-06-26 20:03:48 +0100

    Add functions and class from `src/juml/_old/test_utils.py` to `src/juml/util.py`

--- src/juml/util.py
+++ src/juml/util.py
@@ -1,5 +1,5 @@
-import math
 import torch
+from jutility import util
 
 ZERO = torch.tensor(0.0)
 
@@ -49,3 +49,44 @@ def safe_divide(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
 def error_if_not_finite(x: torch.Tensor):
     if not x.isfinite().all():
         raise RuntimeError()
+
+def all_in_range(x: torch.Tensor, x_lo: float, x_hi: float) -> bool:
+    return (x >= x_lo).all().item() and (x <= x_hi).all().item()
+
+def all_close_to_zero(x: torch.Tensor, tol: float=1e-5) -> bool:
+    return all_in_range(x, -tol, tol)
+
+def all_close(x: torch.Tensor, y: torch.Tensor, tol: float=1e-5) -> bool:
+    return all_close_to_zero(x - y, tol)
+
+def set_torch_seed(*args):
+    seed = util.Seeder().get_seed(*args)
+    torch.manual_seed(seed)
+
+def torch_set_print_options(
+    precision:  int=3,
+    threshold:  (int | float)=1e3,
+    linewidth:  (int | float)=1e5,
+    sci_mode:   bool=False,
+):
+    torch.set_printoptions(
+        precision=precision,
+        threshold=int(threshold),
+        linewidth=int(linewidth),
+        sci_mode=sci_mode,
+    )
+
+class TensorPrinter:
+    def __init__(self, printer: (util.Printer | None)=None):
+        if printer is None:
+            printer = util.Printer()
+
+        self.printer = printer
+
+    def __call__(self, x: torch.Tensor):
+        self.printer.hline()
+        self.printer(
+            "shape = %s\nnumel = %s\ndtype = %s\n%s"
+            % (list(x.shape), x.numel(), x.dtype, x)
+        )
+        self.printer.hline()
